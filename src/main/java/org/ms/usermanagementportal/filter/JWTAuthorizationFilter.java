558483e4417037978f725da7527f59e7db281e69
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-03-18 01:13:52 +0630

    enhancement(UMP): added SecurityConfiguration

--- src/main/java/org/ms/usermanagementportal/filter/JWTAuthorizationFilter.java
+++ src/main/java/org/ms/usermanagementportal/filter/JWTAuthorizationFilter.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -15,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-
+@Component
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
     private JWTTokenProvider jwtTokenProvider;
 
--- src/main/java/org/ms/usermanagementportal/service/impl/UserServiceImpl.java
+++ src/main/java/org/ms/usermanagementportal/service/impl/UserServiceImpl.java
@@ -17,7 +17,7 @@ import java.util.Date;
 
 @Service
 @Transactional
-@Qualifier("UserDetailsService")
+@Qualifier("userDetailsService")
 public class UserServiceImpl implements UserService, UserDetailsService {
     private UserRepository userRepository;
     private Logger LOGGER = LoggerFactory.getLogger(getClass());

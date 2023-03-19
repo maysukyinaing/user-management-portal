89b0dc1be4817478244b135bc078ac1f3f8bcab0
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-03-19 14:18:44 +0630

    enhancement(UMP): added UserServiceImpl

--- src/main/java/org/ms/usermanagementportal/service/UserService.java
+++ src/main/java/org/ms/usermanagementportal/service/UserService.java
@@ -1,4 +1,15 @@
 package org.ms.usermanagementportal.service;
 
+import org.ms.usermanagementportal.exception.domain.EmailExistException;
+import org.ms.usermanagementportal.exception.domain.UserNotFoundException;
+import org.ms.usermanagementportal.exception.domain.UsernameExistException;
+import org.ms.usermanagementportal.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    User register(String firstName, String lastName, String username, String email) throws UserNotFoundException, EmailExistException, UsernameExistException;
+    List<User> getUsers();
+    User findUserByUsername(String username);
+    User findUserByEmail(String email);
 }

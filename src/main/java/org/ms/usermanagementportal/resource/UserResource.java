7cea10160f2741b6e5479849df4b633d037fb1ae
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-03-19 14:19:21 +0630

    enhancement(UMP): added User Register

--- src/main/java/org/ms/usermanagementportal/resource/UserResource.java
+++ src/main/java/org/ms/usermanagementportal/resource/UserResource.java
@@ -3,18 +3,27 @@ package org.ms.usermanagementportal.resource;
 import org.ms.usermanagementportal.exception.domain.EmailExistException;
 import org.ms.usermanagementportal.exception.domain.ExceptionHandling;
 import org.ms.usermanagementportal.exception.domain.UserNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ms.usermanagementportal.exception.domain.UsernameExistException;
+import org.ms.usermanagementportal.model.User;
+import org.ms.usermanagementportal.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = {"/","/user"})
 public class UserResource extends ExceptionHandling {
 
-    @GetMapping("/home")
-    public String showUser() throws UserNotFoundException {
-    //    return "Application works!";
-        throw new UserNotFoundException("This user was not found");
+    private UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, EmailExistException, UsernameExistException {
+        User loginUser =userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
+        return new ResponseEntity<>(loginUser, HttpStatus.OK);
     }
 
 }
